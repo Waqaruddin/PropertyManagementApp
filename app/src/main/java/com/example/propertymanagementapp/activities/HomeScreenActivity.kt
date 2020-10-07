b7c33a43c8d54e5dbf460f4681b884f27c06f362
Syed Waqar Uddin <49626225+Waqaruddin@users.noreply.github.com>
2020-10-06 22:13:57 -0400

    Bottom sheet implemented

--- app/src/main/java/com/example/propertymanagementapp/activities/HomeScreenActivity.kt
+++ app/src/main/java/com/example/propertymanagementapp/activities/HomeScreenActivity.kt
@@ -3,10 +3,23 @@ package com.example.propertymanagementapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.propertymanagementapp.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_home_screen.*
 
 class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        init()
+    }
+
+    private fun init() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val view = layoutInflater.inflate(R.layout.bottom_sheet, null)
+        bottomSheetDialog.setContentView(view)
+        button_add_photo.setOnClickListener {
+            bottomSheetDialog.show()
+        }
     }
 }
